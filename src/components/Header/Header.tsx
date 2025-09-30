e78c63e1dd2b7bc20fab1e0ddc74ed94f5a4503d
SebaR1 <romerosebastian781@gmail.com>
2025-09-29 22:15:55 -0300

    fix: logo header y footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -264,7 +264,7 @@ const Header: React.FC = () => {
             <div className="flex-1 flex justify-start px-4 md:flex-initial">
               <Link to="/">
                 <img 
-                  src="images/logo.png" 
+                  src="/images/logo.png" 
                   alt="Logo" 
                   className="h-9 max-w-20"
                 />
